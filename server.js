e63d86e44a36cff01f85119e8a1f41945ee77f88
SamaraFellaDina <sammybensink@gmail.com>
2024-03-25 10:35:10 +0100

    beoordeling code :)

--- server.js
+++ server.js
@@ -27,13 +27,39 @@ app.get('/', (request, response) => {
 app.get('/artikel/:slug', function (request, response) {
      fetchJson(apiPosts + '?slug=' + request.params.slug).then((apiData) => {
            response.render('article.ejs', {
-            article: apiData
+            article: apiData[0] 
+            // Door [0] te gebruiken hier, hoef je het niet in het ejs
+            // bestand te gebruiken. Hiermee zorg je ervoor dat hij het eerste object pakt
+            
       })
     })
   })
 
+//   maken van post
+let likes = []
+app.post('/artikel/:slug', function (request, response) {
+    let numberOfLikes = likes.find((like) => {
+        return like.slug == request.params.slug
+    })
+
+    if (huidig == undefined) {
+        likes.push({
+            slug: request.params.slug,
+            likes:1,
+        })
+    } 
+
+    else {
+        huidig.likes++
+    }
+
+    console.log(huidig)
+    response.send('post route naar afhandeling')
+    response.redirect(301, '/article/' + request.params.slug)
+})
+
 // Een port aanroepen om alles op te hosten
-app.set('port', process.env.PORT || 8000)
+app.set('port', process.env.PORT || 8080)
 app.listen(app.get('port'), function () {
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
